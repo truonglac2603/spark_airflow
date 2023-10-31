ef7e92c724842149efcb8a60b878e5ac36e360b5
truongngt <truongngt@vnpt.vn>
2023-10-31 11:10:14 +0700

    move dag to outside

---
+++ DAG_spark_test.py
@@ -0,0 +1,29 @@
+import time
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+# from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+
+dag = DAG(
+    dag_id="spark_test",
+    description='Hello World DAG',
+    start_date=datetime(2023, 1, 1),
+    schedule_interval='@once',
+    catchup=False
+)
+spark_submit = BashOperator(
+    task_id = 'spark_test',
+    dag = dag,
+    bash_command='echo lmao'
+)
+# spark_submit = SparkSubmitOperator(
+# 	task_id='spark_test',
+#     conn_id='spark_conn',
+#     application='plugins/spark_1.py',
+#     executor_cores=2,
+#     executor_memory='471859200',
+#     name='spark_submit',
+#     dag=dag
+# )
+
+spark_submit
